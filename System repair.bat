5193b7d46ec128b0f2c8d9a5e43b09bb53e5ae00
HHonzik <87828740+HHonzik@users.noreply.github.com>
2022-10-02 17:42:36 +0200

    Add files via upload

---
+++ System repair.bat
@@ -0,0 +1,28 @@
+@echo off 
+title System repair
+net session >nul 2>&1
+if %errorLevel% == 0 (
+   color 2F
+   echo Running system repair
+) else (
+   COLOR 0f
+   set dir=%cd%
+   goto :admin_get
+   exit
+)
+sfc /scannow
+Dism /Online /Cleanup-Image /CheckHealth
+Dism /Online /Cleanup-Image /ScanHealth
+Dism /Online /Cleanup-Image /RestoreHealth
+sfc /scannow
+echo Done system repair
+echo Press any key to exit.
+pause >nul
+
+:admin_get
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "%dir%\FIX WINDOWS.bat", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
